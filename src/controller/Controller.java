d36802f057c093d57637901fad6868542357e4d8
Nora Blakaj <nora_blakaj@outlook.com>
2020-02-19 23:39:07 +0100

    Implementing the main method.

---
+++ src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Controller {
+
+    public void loop(){
+        //TODO
+    }
+
+    public void setup(){
+        //TODO
+    }
+}
--- src/view/Screen.java
+++ src/view/Screen.java
@@ -3,7 +3,7 @@ package view;
 import java.awt.*;
 import acm.program.GraphicsProgram;
 
-public class Screen extends GraphicsProgram{
+public class Screen extends GraphicsProgram {
 
     private static final Color PLAYER_1_COLOR = new Color(255, 65, 60);
     private static final Color PLAYER_2_COLOR = new Color(255, 247, 97);
