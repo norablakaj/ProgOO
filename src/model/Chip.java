4fce6d1847b9fe54c669fadf1d3d2e31ea93584e
Nora Blakaj <nora_blakaj@outlook.com>
2020-02-21 21:08:00 +0100

    Commentary and assertion added

--- src/model/Chip.java
+++ src/model/Chip.java
@@ -1,14 +1,30 @@
 package model;
 
+/**
+ * Characterizes the chips, which will be added to a board.
+ */
 public class Chip {
 
-    private int player;
+    /**
+     * Chips always belong to a player.
+     */
+    private final int player;
 
+    /**
+     * Constructor for the chip.
+     * @param player it belongs to.
+     */
     public Chip(int player){
 
+        // There should only be two players (can't be played otherwise)
+        assert(player == 1 || player == 0);
         this.player = player;
     }
 
+    /**
+     * Getter.
+     * @return the player.
+     */
     public int getPlayer(){
         return player;
     }
